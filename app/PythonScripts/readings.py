542e35eb4e101bdc38046ed5a8699abded29c608
Qhobeey Martison <bludohric@gmail.com>
2024-07-05 11:36:15 +0000

    windows installation

--- app/PythonScripts/readings.py
+++ app/PythonScripts/readings.py
@@ -1,9 +1,19 @@
 # add_numbers.py
 def readings(comport):
-    list1 = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
-    return random.choice(list1)
-    # return comport
-    # return 400
+    # list1 = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+    # return random.choice(list1)
+    
+    try:
+        serialBout = serial.Serial()
+        serialBout.port = "COM13"
+        serialBout.baudrate = 9600
+        serialBout.open()
+
+        packet = serialBout.readline()
+        wtt = packet.decode("utf").rstrip("\n")
+        return wtt
+    except:
+        return 0
 
 if __name__ == "__main__":
     import sys
